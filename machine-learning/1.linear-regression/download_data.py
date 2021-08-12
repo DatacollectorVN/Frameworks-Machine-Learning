c4a51835b8bcf2e2e12cf0f48bd673b013de678d
DatacollectorVN <datacollectoriu2@gmail.com>
2021-08-12 22:36:17 +0700

    linear-regression

---
+++ machine-learning/1.linear-regression/download_data.py
@@ -0,0 +1,30 @@
+import urllib
+import os
+import requests
+import argparse
+class Cfg(object):
+  
+    def __init__(self):
+        super(Cfg, self).__init__()
+        self.data_urls = ['https://github.com/DatacollectorVN/Frameworks-Machine-Learning/releases/download/3/ames.csv']
+    
+    def down_data(self, destination_dir):
+        data_url = self.data_urls[0]
+        print ('Start to download, this process take a few minutes')
+
+        destination = os.path.join(destination_dir, data_url.split('/')[-1])
+        urllib.request.urlretrieve(data_url, destination)
+        print("Downloaded dataset - {} to- '{}'".format(data_url, destination))
+
+def main(destination_dir):
+    cfg = Cfg()
+    cfg.down_data(destination_dir = destination_dir)
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--dest', help= 'Destination to save model', type= str,
+                        default= './dataset/', dest = 'dest')
+
+    args = parser.parse_args()
+    os.makedirs(args.dest, exist_ok = True)
+    main(destination_dir = args.dest)
